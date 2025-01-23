9bcbaccd460cbd3c00a15cacae663f89a2e941a2
Maya Arafat <mayaarafat95@gmail.com>
2025-01-23 15:21:49 +0100

    Front updates

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template
 import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
 app = Flask(__name__)
 
-# Exemple de DataFrame fictif
-df_clients = pd.DataFrame({
-    'Gender': ['F', 'M', 'F', 'F', 'M', 'M']
-})
+data_path = 'data/BankChurners.csv'
+df = pd.read_csv(data_path)
+
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/dashboard')
+@app.route('/dashboard')    
 def dashboard():
     # Calcul de la répartition par genre
-    gender_counts = df_clients['Gender'].value_counts().to_dict()
+    gender_counts = df['Gender'].value_counts().to_dict()
 
     # Conversion en listes
     gender_labels = list(gender_counts.keys())   # Ex.: ["F", "M"]
