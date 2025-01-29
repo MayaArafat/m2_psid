e6fb7273506948a99715c6eec01e0bafed9634f0
Maya Arafat <mayaarafat95@gmail.com>
2025-01-29 14:04:05 +0100

    graphics

---
+++ analytics_main.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+
+df = pd.read_csv("data/BankChurners.csv")
+
+def init_data() :
+    # Forcer la colonne CLIENTNUM en string
+    df['CLIENTNUM'] = df['CLIENTNUM'].astype(str)
+
+    # Convertir les autres colonnes 'object' en 'category' (sauf CLIENTNUM)
+    for col in df.columns:
+        if df[col].dtype == 'object' and col != 'CLIENTNUM':
+            df[col] = df[col].astype('category')
+
+    # Ajout d'une colonne d'utilisation du crédit (catégorisation)
+    df['Utilization_Range'] = pd.cut(
+        df['Avg_Utilization_Ratio'],
+        bins=[0, 0.25, 0.50, 0.75, 1],
+        labels=['Faible', 'Moyenne', 'Élevée', 'Très élevée']
+    )
+    return df
+
+def encode_data(df):
+    # DataFrame encodé (optionnel)
+    df_encoded = pd.get_dummies(df, drop_first=True)
+    df_encoded = df_encoded.apply(pd.to_numeric, errors='coerce')
+    return df_encoded
+
+# KPI
+def getKpi(df) :
+    nb_total_clients = len(df)
+    taux_attrition = 0.0
+    if 'Attrited Customer' in df['Attrition_Flag'].cat.categories:
+        taux_attrition = (df['Attrition_Flag']
+                            .value_counts(normalize=True)['Attrited Customer'] * 100).round(2)
+    anciennete_moyenne = df['Months_on_book'].mean().round(1)
+
+    return nb_total_clients, taux_attrition, anciennete_moyenne
+
+
+# Repartition des genres (jauge)
+def nbClients_par_genre(df):
+    df_genre = df.groupby('Gender', observed=True).size().reset_index(name='Nombre de clients')
+    fig = px.bar(
+        df_genre,
+        x='Gender',
+        y='Nombre de clients',
+        title="Répartition des clients par Genre",
+        color='Gender',
+        labels={
+            'Gender': "Genre",
+            'Nombre de clients': 'Nombre de clients'
+        }
+    )
+
+    return fig
+
+
+###############################################################################
+# 3) CRÉATION DES VISUALISATIONS SANS FILTRE
+###############################################################################
+
+# A) Répartition des clients par statut d'attrition
+def attrition_client(df) : 
+    df_attrition = df.groupby('Attrition_Flag', observed=True).size().reset_index(name='Nombre de clients')
+    fig_attrition = px.bar(
+        df_attrition,
+        x='Attrition_Flag',
+        y='Nombre de clients',
+        title="Répartition des clients par statut d'attrition",
+        color='Attrition_Flag',
+        labels={
+            'Attrition_Flag': "Statut d'attrition",
+            'Nombre de clients': 'Nombre de clients'
+        }
+    )
+    fig_attrition.update_traces(
+        hovertemplate="<b>Statut</b> : %{x}<br><b>Clients</b> : %{y}"
+    )
+
+    return fig_attrition
+
+# B) Taux d'attrition (camembert)
+def taux_attrition_chart(df, taux_attrition_value): 
+    fig_attrition_gauge = px.pie(
+        values=[taux_attrition_value, 100 - taux_attrition_value],
+        names=['Clients désinscrits', 'Clients existants'],
+        title="Taux d'attrition des clients",
+        hole=0.4
+    )
+    fig_attrition_gauge.update_traces(
+        hovertemplate="<b>%{label}</b> : %{percent}",
+        textinfo='label+percent'
+    )
+    return fig_attrition_gauge
+
+
+# E) Évolution du nombre moyen de transactions
+def evol_nb_moyen_transactions(df) :
+    df_grouped_transactions = df.groupby('Months_on_book', observed=True)['Total_Trans_Ct'].mean().reset_index()
+    fig_transactions = px.line(
+        df_grouped_transactions,
+        x='Months_on_book',
+        y='Total_Trans_Ct',
+        title="Évolution du nombre moyen de transactions",
+        labels={
+            'Months_on_book': 'Ancienneté (mois)',
+            'Total_Trans_Ct': 'Moyenne des transactions'
+        }
+    )
+    return fig_transactions
+
+    # F) Histogramme du taux d'utilisation
+def hist_utilisation(df) :
+    fig_util_hist = px.histogram(
+        df,
+        x='Avg_Utilization_Ratio',
+        color='Attrition_Flag',
+        title="Distribution du taux d'utilisation du crédit",
+        barmode='overlay',
+        nbins=20,
+        labels={
+            'Avg_Utilization_Ratio': 'Taux d\'utilisation du crédit',
+            'Attrition_Flag': 'Statut d\'attrition'
+        }
+    )
+    return fig_util_hist
+
+# G) Montant moyen des transactions par catégorie de carte
+def transac_moyen_by_cat(df) :
+    df_card_mean = df.groupby('Card_Category', observed=True, as_index=False)['Total_Trans_Amt'].mean()
+    df_card_mean.rename(columns={'Total_Trans_Amt': 'Montant moyen des transactions'}, inplace=True)
+    fig_card_mean = px.bar(
+        df_card_mean,
+        x='Card_Category',
+        y='Montant moyen des transactions',
+        title="Montant moyen des transactions par catégorie de carte",
+        labels={
+            'Card_Category': 'Type de carte',
+            'Montant moyen des transactions': 'Montant moyen'
+        }
+    )
+    return fig_card_mean
+
+# H) Sunburst (Revenu → Carte)
+def sunburst_revenu_cart(df) :
+    df_sun = df.groupby(['Income_Category', 'Card_Category'], observed=True).size().reset_index(name='Count')
+    fig_sunburst = px.sunburst(
+        df_sun,
+        path=['Income_Category', 'Card_Category'],
+        values='Count',
+        title="Répartition hiérarchique Revenus → Carte",
+        labels={
+            'Income_Category': 'Revenus',
+            'Card_Category': 'Carte'
+        }
+    )
+    return fig_sunburst
+
+# I) Parallel Categories
+def get_parallel_categories(df) :
+    df_parallel = df[['Attrition_Flag', 'Income_Category', 'Card_Category']].dropna()
+    fig_parallel = px.parallel_categories(
+        df_parallel,
+        dimensions=['Attrition_Flag', 'Income_Category', 'Card_Category'],
+        title="Analyse en parallèle (Attrition, Revenus, Carte)"
+    )
+    return fig_parallel
--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from dashboard import init_dashboard  # on importe la fonction
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import plotly.express as px
+import plotly.io as pio
+from analytics_main import *
 
 app = Flask(__name__)
-
-data_path = 'data/BankChurners.csv'
-df = pd.read_csv(data_path)
-
 
 @app.route('/')
 def home():
@@ -21,17 +20,46 @@ def dashboard():
 
 @app.route('/dashboard-front')
 def front_dashboard():
-    gender_counts = df['Gender'].value_counts().to_dict()
+    df = init_data()    
+    nb_total_clients, taux_attrition_value, anciennete_moyenne = getKpi(df)
+    
+    # Génération des graphiques
+    fig_attrition = attrition_client(df)
+    fig_taux_attrition = taux_attrition_chart(df, taux_attrition_value)
+    fig_transactions = evol_nb_moyen_transactions(df)
+    fig_hist_utilisation = hist_utilisation(df)
+    fig_transac_moyen_by_cat = transac_moyen_by_cat(df)
+    fig_sunburst_revenu_cart = sunburst_revenu_cart(df)
+    fig_parallel_categories = get_parallel_categories(df)
+    fig_nbclients_par_genre = nbClients_par_genre(df)
 
-    # Conversion en listes
-    gender_labels = list(gender_counts.keys())   # Ex.: ["F", "M"]
-    gender_values = list(gender_counts.values()) # Ex.: [3, 3]
+    # Conversion en JSON pour transmission au template
+    graph_fig_attrition = pio.to_json(fig_attrition)
+    graph_taux_attrition = pio.to_json(fig_taux_attrition)
+    graph_transactions = pio.to_json(fig_transactions, pretty=True)
+    graph_hist_utilisation = pio.to_json(fig_hist_utilisation)
+    graph_transac_moyen_by_cat = pio.to_json(fig_transac_moyen_by_cat)
+    graph_sunburst_revenu_cart = pio.to_json(fig_sunburst_revenu_cart)
+    graph_parallel_categories = pio.to_json(fig_parallel_categories)
+    graph_nbclients_par_genre = pio.to_json(fig_nbclients_par_genre)
+
 
     return render_template(
         'dashboard.html',
-        gender_labels=gender_labels,
-        gender_values=gender_values
+        kpi1=nb_total_clients,
+        kpi2=taux_attrition_value,
+        kpi3=anciennete_moyenne,
+        fig_attrition_json=graph_fig_attrition,
+        taux_attrition_json=graph_taux_attrition,
+        transactions_json=graph_transactions,
+        hist_utilisation_json = graph_hist_utilisation,
+        transac_moyen_by_cat_json = graph_transac_moyen_by_cat,
+        sunburst_revenu_cart_json = graph_sunburst_revenu_cart,
+        parallel_categories_json = graph_parallel_categories,
+        graph_nbclients_par_genre_json = graph_nbclients_par_genre
     )
+
+
 
 if __name__ == '__main__':
     # On appelle init_dashboard(app) pour monter Dash sur /dash/
